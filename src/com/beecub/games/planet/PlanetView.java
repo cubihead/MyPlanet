12fae27a937f8111e4fc7ce6bcf1001b5d5e68af
beecub <beecub@beecub.com>
2011-12-02 16:15:48 +0100

    -

--- src/com/beecub/games/planet/PlanetView.java
+++ src/com/beecub/games/planet/PlanetView.java
@@ -13,7 +13,6 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
--- src/com/beecub/games/planet/TasksActivity.java
+++ src/com/beecub/games/planet/TasksActivity.java
@@ -25,7 +25,7 @@ import android.widget.TextView;
 public class TasksActivity extends Activity {
     
     public ArrayList<Task> mTasks = new ArrayList<Task>();
-    public ListViewAdapter madapter;
+    public ListViewAdapter mAdapter;
     private int currentPosition;
     private float oldTouchValue;
 
@@ -66,6 +66,42 @@ public class TasksActivity extends Activity {
     public void onResume() {
         super.onResume();
         
+        mTasks = new ArrayList<Task>();
+        this.mAdapter = new ListViewAdapter(this, R.layout.row, mTasks);
+        //mAdapter.notifyDataSetChanged();
+        
+        ListView lv = (ListView) findViewById(R.id.list_task);
+        lv.setAdapter(mAdapter);
+        lv.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> a, View v, int position, long id) {
+                
+                currentPosition = position;
+                //finish();
+                
+            }
+        });
+        lv.setSelection(currentPosition);
+        
+        Resources res = getResources();
+        String[] tasks = res.getStringArray(R.array.resource);
+        mAdapter.clear();
+        
+        int i = 0;
+        while(i < tasks.length) {
+            int resID = getResources().getIdentifier("task_resource_" + tasks[i], "drawable", getPackageName());
+            Drawable icon;
+            if(resID == 0) 
+                icon = res.getDrawable(R.drawable.icon);
+            else
+                icon = res.getDrawable(resID);
+            
+            mAdapter.add(new Task(Integer.valueOf(tasks[i]), tasks[i+1], tasks[i+2], Long.valueOf(tasks[i+3]), 
+                    +Long.valueOf(tasks[i+4]), -Long.valueOf(tasks[i+5]), -Long.valueOf(tasks[i+6]), -Long.valueOf(tasks[i+7]),
+                    0, icon));
+            i+=8;
+        }
+        
+        
         ImageButton b0 = (ImageButton) findViewById(R.id.Button0);
         ImageButton b1 = (ImageButton) findViewById(R.id.Button1);
         ImageButton b2 = (ImageButton) findViewById(R.id.Button2);
@@ -76,50 +112,70 @@ public class TasksActivity extends Activity {
             @Override
             public void onClick(View arg0) {
                 Log.v("beecub", "onclick0");
+                Resources res = getResources();
+                String[] tasks = res.getStringArray(R.array.resource);
+                mAdapter.clear();
+                
+                int i = 0;
+                while(i < tasks.length) {
+                    int resID = getResources().getIdentifier("task_resource_" + tasks[i], "drawable", getPackageName());
+                    Drawable icon;
+                    if(resID == 0) 
+                        icon = res.getDrawable(R.drawable.icon);
+                    else
+                        icon = res.getDrawable(resID);
+                    
+                    mAdapter.add(new Task(Integer.valueOf(tasks[i]), tasks[i+1], tasks[i+2], Long.valueOf(tasks[i+3]), 
+                            +Long.valueOf(tasks[i+4]), -Long.valueOf(tasks[i+5]), -Long.valueOf(tasks[i+6]), -Long.valueOf(tasks[i+7]),
+                            0, icon));
+                    i+=8;
+                }
+                mAdapter.notifyDataSetChanged();
             }
         });
         
-        
-        mTasks = new ArrayList<Task>();
-        this.madapter = new ListViewAdapter(this, R.layout.row, mTasks);
-        //madapter.notifyDataSetChanged();
-        
-        ListView lv = (ListView) findViewById(R.id.list_task);
-        lv.setAdapter(madapter);
-        lv.setOnItemClickListener(new OnItemClickListener() {
-            public void onItemClick(AdapterView<?> a, View v, int position, long id) {
+        b1.setOnClickListener(new OnClickListener() {
+                    
+            @Override
+            public void onClick(View arg0) {
+                Log.v("beecub", "onclick1");
+                Resources res = getResources();
+                String[] tasks = res.getStringArray(R.array.resource);
+                mAdapter.clear();
                 
-//                    Intent intent = new Intent(GOLauncherFontsActivity.this, DetailedPreviewActivity.class);
-//                    Bundle b = new Bundle();
-//                    b.putString("name", madapter.getItem(position).getName());
-//                    b.putString("author", madapter.getItem(position).getAuthor());
-//                    b.putString("license", madapter.getItem(position).getLicense());
-//                    b.putString("link", madapter.getItem(position).getLink());
-//                    b.putString("typeface", madapter.getItem(position).getTypeface());
-//                    intent.putExtras(b);
-//                    startActivity(intent);
-                
-                currentPosition = position;
-                //finish();
-                
+                int i = 0;
+                while(i < tasks.length) {
+                    int resID = getResources().getIdentifier("task_culture_" + tasks[i], "drawable", getPackageName());
+                    Drawable icon;
+                    if(resID == 0) 
+                        icon = res.getDrawable(R.drawable.icon);
+                    else
+                        icon = res.getDrawable(resID);
+                    
+                    mAdapter.add(new Task(Integer.valueOf(tasks[i]), tasks[i+1], tasks[i+2], Long.valueOf(tasks[i+3]), 
+                            +Long.valueOf(tasks[i+4]), -Long.valueOf(tasks[i+5]), -Long.valueOf(tasks[i+6]), -Long.valueOf(tasks[i+7]),
+                            0, icon));
+                    i+=8;
+                }
+                mAdapter.notifyDataSetChanged();
             }
         });
-        lv.setSelection(currentPosition);
         
-        Resources res = getResources();
-        String[] technologies = res.getStringArray(R.array.technologies);
+        b2.setOnClickListener(new OnClickListener() {
+            
+            @Override
+            public void onClick(View arg0) {
+                Log.v("beecub", "onclick2");                
+            }
+        });
         
-        int i = 6;
-        while(i < technologies.length) {
-            int resID = getResources().getIdentifier("research_" + technologies[i], "drawable", getPackageName());
-            Drawable icon;
-            if(resID == 0) 
-                icon = res.getDrawable(R.drawable.icon);
-            else
-                icon = res.getDrawable(resID);
-            madapter.add(new Task(Integer.valueOf(technologies[i]), technologies[i+1], technologies[i+2], Integer.valueOf(technologies[i+3]), Long.valueOf(technologies[i+4]), Long.valueOf(technologies[i+5]), icon));
-            i = i + 6;
-        }
+        b3.setOnClickListener(new OnClickListener() {
+            
+            @Override
+            public void onClick(View arg0) {
+                Log.v("beecub", "onclick3");
+            }
+        });
     }
     
     public class ListViewAdapter extends ArrayAdapter<Task> {
@@ -140,9 +196,12 @@ public class TasksActivity extends Activity {
             Task o = items.get(position);
             if (o != null) {
                 TextView tv1 = (TextView) v.findViewById(R.id.name);
-                TextView tv2 = (TextView) v.findViewById(R.id.duration);
+                TextView tv2 = (TextView) v.findViewById(R.id.amount);
                 TextView tv3 = (TextView) v.findViewById(R.id.cost);
-                TextView tv4 = (TextView) v.findViewById(R.id.description);
+                TextView tv4 = (TextView) v.findViewById(R.id.resource);
+                TextView tv5 = (TextView) v.findViewById(R.id.mood);
+                TextView tv6 = (TextView) v.findViewById(R.id.energy);
+                TextView tv7 = (TextView) v.findViewById(R.id.environment);
                 ImageView iv1 = (ImageView) v.findViewById(R.id.rowicon);
                 
                 if(tv1 != null) {
@@ -150,7 +209,7 @@ public class TasksActivity extends Activity {
                     tv1.setTypeface(PlanetActivity.mTypeface);
                 }
                 if(tv2 != null) {
-                    tv2.setText(String.valueOf(o.getDuration()));
+                    tv2.setText(String.valueOf(o.getAmount()));
                     tv2.setTypeface(PlanetActivity.mTypeface);
                 }
                 if(tv3 != null) {
@@ -158,8 +217,20 @@ public class TasksActivity extends Activity {
                     tv3.setTypeface(PlanetActivity.mTypeface);
                 }
                 if(tv4 != null) {
-                    tv4.setText(o.getDescription());
+                    tv4.setText(String.valueOf(o.getResource()) + getResources().getString(R.string.perhour));
                     tv4.setTypeface(PlanetActivity.mTypeface);
+                }
+                if(tv5 != null) {
+                    tv5.setText(String.valueOf(o.getMood()));
+                    tv5.setTypeface(PlanetActivity.mTypeface);
+                }
+                if(tv6 != null) {
+                    tv6.setText(String.valueOf(o.getEnergy()));
+                    tv6.setTypeface(PlanetActivity.mTypeface);
+                }
+                if(tv7 != null) {
+                    tv7.setText(String.valueOf(o.getEnvironment()));
+                    tv7.setTypeface(PlanetActivity.mTypeface);
                 }
                 if(iv1 != null) {
                     iv1.setBackgroundDrawable(o.getIcon());
@@ -171,21 +242,29 @@ public class TasksActivity extends Activity {
     }
     
     public class Task {
-        public int mNumber;
-        public String mName;
-        public String mDescription;
-        public int mLevel;
-        public long mDuration;
-        public long mCost;
-        Drawable mIcon;
+        private int mNumber;
+        private String mName;
+        private String mDescription;
+        private long mCost;
+        private long mImpactResource;
+        private long mImpactMood;
+        private long mImpactEnergy;
+        private long mImpactEnvironment;
+        private int mAmount;
+        private Drawable mIcon;
         
-        public Task(int number, String name, String description, int level, long duration, long cost, Drawable image) {
+        public Task(int number, String name, String description, long cost, 
+                long impactResource, long impactMood, long impactEnergy, long impactEnvironment, 
+                int amount, Drawable image) {
             this.mNumber = number;
             this.mName = name;
             this.mDescription = description;
-            this.mLevel = level;
-            this.mDuration = duration;
             this.mCost = cost;
+            this.mImpactResource = impactResource;
+            this.mImpactMood = impactMood;
+            this.mImpactEnergy = impactEnergy;
+            this.mImpactEnvironment = impactEnvironment;
+            this.mAmount = amount;
             this.mIcon = image;
         }
         
@@ -198,14 +277,27 @@ public class TasksActivity extends Activity {
         public String getDescription() {
             return this.mDescription;
         }
-        public int getLevel() {
-            return this.mLevel;
-        }
-        public long getDuration() {
-            return this.mDuration;
-        }
         public long getCost() {
             return this.mCost;
+        }
+        public long getResource() {
+            return this.mImpactResource;
+        }
+        public long getMood() {
+            return this.mImpactMood;
+        }
+        public long getEnergy() {
+            return this.mImpactEnergy;
+        }
+        public long getEnvironment() {
+            return this.mImpactEnvironment;
+        }
+        public int getAmount() {
+            return this.mAmount;
+        }
+        public void setAmount(int amount) {
+            //* TODO save amount to disk
+            this.mAmount = amount;
         }
         public Drawable getIcon() {
             return this.mIcon;
